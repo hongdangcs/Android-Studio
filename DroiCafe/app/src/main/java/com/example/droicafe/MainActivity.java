2225bdd92633df2e09097b8ed2d5efe10f228867
hongdangcseiu <hongdang2k1@gmail.com>
2022-10-19 10:22:24 +0700

    droid cafe shopping card ic

--- DroiCafe/app/src/main/java/com/example/droicafe/MainActivity.java
+++ DroiCafe/app/src/main/java/com/example/droicafe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.droicafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -36,8 +37,9 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this,
+                        OrderActivity.class);
+                startActivity(intent);
             }
         });
     }
--- DroiCafe/app/src/main/java/com/example/droicafe/OrderActivity.java
+++ DroiCafe/app/src/main/java/com/example/droicafe/OrderActivity.java
@@ -2,7 +2,9 @@ package com.example.droicafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -11,4 +13,5 @@ public class OrderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
     }
+
 }
